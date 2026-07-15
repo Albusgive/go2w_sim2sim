5252878e0a66c48b926af59db1b8dc80f6244786
Albusgive <mb1874392095@163.com>
2026-07-15 11:08:32 +0800

    chore: remove committed build artifacts and ignore build directories

--- mujoco/C++/build/CMakeFiles/lab2mj.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for lab2mj.
-# This may be replaced when dependencies are built.
--- mujoco/C++/build_onnx/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/albusgive2/go2w_sim2sim/mujoco/C++
-# Build directory: /home/albusgive2/go2w_sim2sim/mujoco/C++/build_onnx
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
